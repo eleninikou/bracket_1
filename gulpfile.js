034e4f4936338dee4f2d9e613c5bc9cfc4928b72
Thanos Eleftherakos <teleftherakos@gmail.com>
2016-04-15 20:13:56 +0300

    add vinyl ftp,file include

--- gulpfile.js
+++ gulpfile.js
@@ -14,33 +14,53 @@ var gulp            = require("gulp"),
     streamify       = require('gulp-streamify'),
     browserify      = require("browserify"),
     rename          = require('gulp-rename'),
-    debowerify      =require('debowerify');
+    debowerify      = require('debowerify'),
+    cssImport       = require('gulp-cssimport'),
+    fileinclude     = require('gulp-file-include'),
+    gutil           = require( 'gulp-util' ),
+    ftp             = require( 'vinyl-ftp' );
 
 // -----------------------------------------------------------------------------
+var config = {
+     bowerDir    : './bower_components',
+    dest        : 'dist',
+    dest_js     : 'dist/assets/js',
+    dest_css    : 'dist/assets/css',
+    dest_html   : 'dist/*.html',
+    dest_assets : 'dist/assets',
+    src         : 'src',
+    src_html    : 'src/*.html',
+    src_partials: 'src/**/*.html',
+    src_sass    : 'src/sass/**/*.scss',
+    src_js      : 'src/js/*.js',
+    src_img     : 'src/img/*'
+}
 
-var dest_js   = "dist/js";
-var dest_css  = "dist/css";
-var dest_html  = "dist/**/*.html";
-
-var src_sass  = "src/sass/**/*.scss";
-var src_js    = "src/js/**/*.js";
-var src_img   = "src/img/*"
 
 // -----------------------------------------------------------------------------
 // SASS TO CSS
 // -----------------------------------------------------------------------------
 gulp.task("sass", function(){
-  return gulp.src(src_sass)
+  return gulp.src(config.src_sass)
               .pipe(plumber({errorHandler: notify.onError("Error: <%= error.message %>")}))
               .pipe(sass())
+              .pipe(cssImport())
               .pipe(prefix('last 3 versions'))
               .pipe(concat('main.min.css'))
-              .pipe(gulp.dest(dest_css))
+              .pipe(gulp.dest(config.dest_css))
               .pipe(minify_css())
-              .pipe(sourcemaps.init())
-              .pipe(sourcemaps.write())
-              .pipe(gulp.dest(dest_css))
+              // .pipe(sourcemaps.init())
+              // .pipe(sourcemaps.write())
+              .pipe(gulp.dest(config.dest_css))
               .pipe(browserSync.reload({stream:true}));
+});
+
+// -----------------------------------------------------------------------------
+// Fonts
+// -----------------------------------------------------------------------------
+gulp.task('fonts', function() { 
+    return gulp.src(config.src + '/fonts/**.*') 
+        .pipe(gulp.dest(config.dest_assets+'/fonts')); 
 });
 
 // -----------------------------------------------------------------------------
@@ -53,8 +73,30 @@ gulp.task('browserify', function() {
     .pipe(source('index.js'))
     .pipe(streamify(uglify()))
     .pipe(rename('bundle.js'))
-    .pipe(gulp.dest(dest_js))
+    .pipe(gulp.dest(config.dest_js))
+    .pipe(browserSync.reload({stream:true}));
 });
+
+// -----------------------------------------------------------------------------
+// Images
+// -----------------------------------------------------------------------------
+gulp.task('images', function() { 
+    return gulp.src(config.src + '/images/**.*') 
+        .pipe(gulp.dest(config.dest_assets+'/images')); 
+});
+
+// -----------------------------------------------------------------------------
+// Fileinclude
+// -----------------------------------------------------------------------------
+gulp.task('fileinclude', function() {
+  gulp.src(config.src_html)
+    .pipe(fileinclude({
+      prefix: '@@',
+      basepath: config.src+'/partials/'
+    }))
+    .pipe(gulp.dest(config.dest));
+});
+
 
 // -----------------------------------------------------------------------------
 // Watch
@@ -63,12 +105,40 @@ gulp.task('watch', function(){
   browserSync.init({
     server: './dist'
   });
-  gulp.watch(src_js, ['browserify']);
-  gulp.watch(src_sass, ['sass']);
-  gulp.watch(dest_html).on('change',browserSync.reload);
+  gulp.watch(config.src_html, ['fileinclude']);
+  gulp.watch(config.src_partials, ['fileinclude']);
+  gulp.watch(config.src_js, ['browserify']);
+  gulp.watch(config.src_sass, ['sass']);
+  gulp.watch(config.dest_html).on('change',browserSync.reload);
 });
 
 // -----------------------------------------------------------------------------
+// FTP Deploy
+// -----------------------------------------------------------------------------
+gulp.task( 'deploy', function () {
+
+	var conn = ftp.create( {
+		host:     '',
+		user:     '',
+		password: '',
+		parallel: 10,
+		log:      gutil.log
+	} );
+
+	var globs = [
+    config.dest+'/**',
+	];
+
+	// using base = '.' will transfer everything to /public_html correctly
+	// turn off buffering in gulp.src for best performance
+
+	return gulp.src( globs, { base: 'dist/', buffer: false } )
+		.pipe( conn.newer( '' ) ) // only upload newer files
+		.pipe( conn.dest( '' ) );
+
+} );
+// -----------------------------------------------------------------------------
 //Default
 // -----------------------------------------------------------------------------
-gulp.task('default',['watch','sass','browserify'])
+gulp.task('default',['watch','sass','fonts','fileinclude','browserify']);
+// gulp.task('default',['watch']);
