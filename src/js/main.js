ed0c0858978f5829d5c02e6c285b09d776f15cdf
Eleni Nikou <eleni.nikou@chasacademy.se>
2018-10-08 16:43:28 +0300

    first commit

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,27 @@
 window.jQuery = $ = require('jquery');
+require('owl.carousel');
 
 $(document).ready(function(){
-  
+
+    $('.owl-carousel').owlCarousel({
+        nav: true, 
+        autoHeight: true,
+        slideSpeed : 300,
+        paginationSpeed : 400,
+        singleItem: true,
+        items: 1,
+        touchDrag: true,
+        dots: true,
+
+    })
+
+    $('.backdrop__nav__topmenu__hamburger').click(function () {
+        console.log('hej')
+        $('.navigation_menu').addClass('show');
+    })
+
+    $('.navigation_menu__close').click(function () {
+        $('.navigation_menu').removeClass('show');
+    })
+
 });
